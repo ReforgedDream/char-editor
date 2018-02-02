25dc79b4bfdc2c1f40b85b743552a113a6e95797
Reforged Dream <reforgeddream@gmail.com>
2018-02-02 23:18:08 +0700

    A simpliest SWT GUI implemented

--- src/main/java/com/reforgeddream/MainClass.java
+++ src/main/java/com/reforgeddream/MainClass.java
@@ -1,8 +1,13 @@
 package com.reforgeddream;
 
+import com.reforgeddream.gui.GuiClass;
+
 public class MainClass {
 
 	public static void main(String[] args) {
+		
+		GuiClass gui = new GuiClass();
+		gui.createDisplay();
 		
 	}
 
